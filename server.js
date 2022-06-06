21524b8e49920ddb14ded60df8fbcb10fd73a0df
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-06-06 10:59:39 +0530

    apollo server interation

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
+const { ApolloServer } = require('apollo-server-express')
 
 const { loadFilesSync } = require('@graphql-tools/load-files')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 
-const app = express()
-
 const typesArray = loadFilesSync('**/*.graphql')
 const resolversArray = loadFilesSync('**/*.resolvers.js')
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolversArray,
-})
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
+const startApolloServer = async () => {
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
   })
-)
 
-app.listen(5000, () => console.log('Running GraphQL server...'))
+  const server = new ApolloServer({
+    schema,
+  })
+
+  const app = express()
+
+  await server.start()
+
+  server.applyMiddleware({ app })
+
+  app.listen({ port: 8000 }, () => console.log('Running GraphQL server...'))
+}
+
+startApolloServer()
